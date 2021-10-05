47c6e9d12b3154f659830606c8c0e7e6f7780e02
audukehinde <auduomohkehinde@gmail.com>
2021-10-05 05:38:45 -0400

    Add mobile navigation property

---
+++ main.js
@@ -0,0 +1,6 @@
+function showMenu() {
+  document.querySelector('.menus').classList.toggle('active');
+  document.querySelector('.fa-bars').classList.toggle('hide');
+  document.querySelector('.fa-times').classList.toggle('show');
+
+}
