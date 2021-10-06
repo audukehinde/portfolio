fe6f4e08535a092ddd8535b007d3af1affc4a5f1
audukehinde <auduomohkehinde@gmail.com>
2021-10-06 15:19:32 -0400

    Add padding to input form

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const navMenu = document.querySelector('.nav-menu');
 const close = document.querySelector('.close');
 const open = document.querySelector('.fa-bars');
 const navLink = document.querySelectorAll('.nav-link');
+const btnProject = document.querySelector('.btnsee');
+// Initializing our body
+const bodyElement = document.body;
+
+
 
 hamburger.addEventListener('click', mobileMenu);
 close.addEventListener('click', switchIcons);
@@ -29,3 +34,23 @@ function switchIcons() {
 function toggleDisplay(element, val) {
   element.style.display = val;
 }
+
+
+// Pop ups
+
+let projectData = {
+    projectTitle : 'MARKETING AGENCY',
+    projectDescription :"A website created for a digital marketing agency to help improve sales and give information about company's product. This website also helps the client to contact the company for a business deal and showcases the company's different services and packages",
+    projectLanguages : {
+        firstLang : 'CSS',
+        secondLag : 'Html',
+        thirdLang : 'JavaScript',
+        fourthLang : 'Ruby'
+    }
+};
+btnProject.addEventListener('click', console.log(projectData.projectLanguages));
+
+// console.log(projectData.projectLanguages.thirdLang);
+
+
+let modal = document.createElement('div');
