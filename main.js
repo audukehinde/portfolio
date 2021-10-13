f381d814ff9d9fc8a96a78dab3ff8391432669a7
audukehinde <auduomohkehinde@gmail.com>
2021-10-13 18:28:09 -0400

    Fix conflict

--- main.js
+++ main.js
@@ -4,14 +4,8 @@ const close = document.querySelector('.close');
 const open = document.querySelector('.fa-bars');
 const navLink = document.querySelectorAll('.nav-link');
 
-hamburger.addEventListener('click', mobileMenu);
-close.addEventListener('click', switchIcons);
-navLink.forEach((n) => n.addEventListener('click', closeMenu));
-
-function mobileMenu() {
-  navMenu.classList.toggle('active');
-  toggleDisplay(open, 'none');
-  toggleDisplay(close, 'block');
+function toggleDisplay(element, val) {
+  element.style.display = val;
 }
 
 function closeMenu() {
@@ -20,15 +14,21 @@ function closeMenu() {
   toggleDisplay(close, 'none');
 }
 
+function mobileMenu() {
+  navMenu.classList.toggle('active');
+  toggleDisplay(open, 'none');
+  toggleDisplay(close, 'block');
+}
+
 function switchIcons() {
   toggleDisplay(open, 'block');
   toggleDisplay(close, 'none');
   closeMenu();
 }
 
-function toggleDisplay(element, val) {
-  element.style.display = val;
-}
+hamburger.addEventListener('click', mobileMenu);
+close.addEventListener('click', switchIcons);
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
 
 // Form Vaidation
 const form = document.getElementById('form');
