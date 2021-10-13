5bfa0c20b62561dfe468afbc7eb5610addc6ebf3
audukehinde <auduomohkehinde@gmail.com>
2021-10-13 06:26:51 -0400

    Implemented the changes requested by the last reviewer

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const close = document.querySelector('.close');
   const open = document.querySelector('.fa-bars');
   const navLink = document.querySelectorAll('.nav-link');
-  const form = document.getElementById('form');
-  const mail = document.getElementById('my-email');
-  const name = document.getElementById('full-name');
-  const msg = document.getElementById('my-message');
   const modal = document.getElementById('card');
 
   function toggleDisplay(element, val) {
@@ -44,35 +40,6 @@ document.addEventListener('DOMContentLoaded', () => {
   hamburger.addEventListener('click', mobileMenu);
   close.addEventListener('click', switchIcons);
   navLink.forEach((n) => n.addEventListener('click', closeMenu));
-
-  // Form Validation
-  form.addEventListener('submit', (e) => {
-    const regx = /^[a-z0-9.!#$%&'+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)$/;
-    const myMail = mail.value;
-    if (myMail.match(regx)) {
-      document.querySelector('.error-Message').innerHTML = '';
-    } else {
-      e.preventDefault();
-      document.querySelector('.error-Message').innerHTML = 'Please remove all Uppercase Letter(s) from Email Field';
-    }
-  });
-  // localStorage
-  const addDataToLocalStorage = () => {
-    const myFormData = {
-      fullName: name.value,
-      email: mail.value,
-      message: msg.value,
-    };
-    localStorage.setItem('myFormData', JSON.stringify(myFormData));
-  };
-  form.addEventListener('change', addDataToLocalStorage);
-
-  let getLocalStorageData = localStorage.getItem('myFormData');
-  getLocalStorageData = JSON.parse(getLocalStorageData);
-  document.getElementById('full-name').value = getLocalStorageData.fullName;
-  document.getElementById('my-email').value = getLocalStorageData.email;
-  document.getElementById('my-message').value = getLocalStorageData.message;
-  document.getElementById('btn-submit').addEventListener('click', addDataToLocalStorage);
 
   // Generating HTML content using JavaScript
   const projectSection = document.getElementById('portfolio');
