2356e00e5fe220112961a0b0667c4e3c61ed1b23
audukehinde <auduomohkehinde@gmail.com>
2021-10-14 04:10:19 -0400

    Fix project section not showing

--- main.js
+++ main.js
@@ -43,3 +43,144 @@ form.addEventListener('submit', (e) => {
     document.querySelector('.error-Message').innerHTML = 'Please remove all Uppercase Letter(s) from Email Field';
   }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navMenu = document.querySelector('.nav-menu');
+  const close = document.querySelector('.close');
+  const open = document.querySelector('.fa-bars');
+  const navLink = document.querySelectorAll('.nav-link');
+  const modal = document.getElementById('card');
+
+  function toggleDisplay(element, val) {
+    element.style.display = val;
+  }
+  function mobileMenu() {
+    navMenu.classList.toggle('active');
+    toggleDisplay(open, 'none');
+    toggleDisplay(close, 'block');
+  }
+
+  function closeMenu() {
+    navMenu.classList.remove('active');
+    toggleDisplay(open, 'block');
+    toggleDisplay(close, 'none');
+  }
+
+  function switchIcons() {
+    toggleDisplay(open, 'block');
+    toggleDisplay(close, 'none');
+    closeMenu();
+  }
+
+  function activateProjectLinks() {
+    const allbutton = document.querySelectorAll('.project-link');
+    allbutton.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        modal.classList.toggle('card');
+        modal.style.display = 'block';
+      });
+    });
+  }
+
+  hamburger.addEventListener('click', mobileMenu);
+  close.addEventListener('click', switchIcons);
+  navLink.forEach((n) => n.addEventListener('click', closeMenu));
+
+  // Generating HTML content using JavaScript
+  const projectSection = document.getElementById('portfolio');
+  const projectSectionTitle = document.createElement('h2');
+  const underLineImage = document.createElement('img');
+  const projectCard = document.createElement('div');
+  underLineImage.src = 'img/Vector4.png';
+  underLineImage.setAttribute('alt', 'Project Image');
+  projectSectionTitle.innerText = 'MY RECENT PROJECT';
+  projectSection.append(projectSectionTitle, underLineImage);
+  const projectData = [
+    4,
+    {
+      projectTitle: '',
+      projectDescription: "A website created for a digital marketing agency to help improve sales and give information about company's product. This website also helps the client to contact the company for a business deal and showcases the company's different services and packages.",
+    },
+    {
+      firstLang: 'CSS',
+      secondLang: 'HTML',
+      thirdLang: 'JavaScript',
+      fourthLang: 'Ruby',
+    },
+    {
+      imag1: 'img/fola2.PNG',
+      image2: 'img/fola3.PNG',
+      image3: 'img/spike.PNG',
+      image4: 'img/fola2.PNG',
+    },
+  ];
+
+  for (let i = 1; i <= projectData[0]; i += 1) {
+    const projects = `
+    <div class="proof proof-space">
+      <img id="project-image${i}" class="project-img" src="" alt="Project33" />
+      <div class="inner-element">
+        <h3>MARKETING AGENCY</h3>
+        <p>${projectData[1].projectDescription}</p>
+        <div class="tools">
+          <ul>
+            <li class="line">${projectData[2].firstLang}</li>
+            <li class="line">${projectData[2].secondLang}</li>
+            <li class="line">${projectData[2].thirdLang}</li>
+            <li>${projectData[2].fourthLang}</li>
+          </ul>
+        </div>
+        <button class="project-link btnsee btncheck">See Project</button>
+      </div>
+    </div>`;
+    projectSection.innerHTML += projects;
+  }
+  document.getElementById('project-image1').src = projectData[3].imag1;
+  document.getElementById('project-image2').src = projectData[3].image2;
+  document.getElementById('project-image3').src = projectData[3].image3;
+  document.getElementById('project-image4').src = projectData[3].image4;
+
+  projectSectionTitle.className = 'recent';
+  underLineImage.className = 'line-divider2';
+  projectCard.className = 'inner-element';
+
+  const popupCard = document.getElementById('card');
+  const popCard = `<div class="card-header">
+    <h2>Multi Post Story</h2>
+  <div class="class-close">&times;</div>
+  </div>
+  <img src="img/card-img.png" class="card-img" alt="">
+  <p class="card-desc">${projectData[1].projectDescription}</p>
+    <div class="tool">
+            <ul>
+              <li class="line">${projectData[2].firstLang}</li>
+              <li class="line">${projectData[2].secondLang}</li>
+              <li class="line">${projectData[2].thirdLang}</li>
+              <li>${projectData[2].fourthLang}</li>
+            </ul>
+    </div>
+    <div class="card-button">
+      <button class="project-link btnsee">See Live <a href="#"><i class="fab fa-github"></i></a></button>
+      <button class="project-link btnsee">See Source <i class="fab fa-github"></i></a></button>
+    </div>
+    <div id="overlay"></div>`;
+  popupCard.innerHTML += popCard;
+
+  // CLOSE PROJECT PREVIEW
+  const closeButton = document.querySelector('.class-close');
+  closeButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+    activateProjectLinks();
+  });
+
+  // project opener buttons
+  const openProjectBtns = document.querySelectorAll('btncheck');
+  openProjectBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+      modal.classList.toggle('card');
+      activateProjectLinks();
+    });
+  });
+  activateProjectLinks();
+});
